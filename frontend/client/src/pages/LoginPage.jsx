c338cba1a160b6529e327937f0375198ddcc24af
Thakurdeepu <deeputhakur73378@gmail.com>
2025-10-26 19:32:29 +0530

    add handlesubmit function

--- frontend/client/src/pages/LoginPage.jsx
+++ frontend/client/src/pages/LoginPage.jsx
@@ -1,4 +1,12 @@
-  const LoginPage = () => {
+import { useState } from "react";
+const LoginPage = () => {
+    const [formData, setFormData] = useState({
+      email : "",
+      password : ""
+    })
+    const handleSubmit = () =>{
+      console.alert("You are loggin")      
+    }
   return <div>
     <div className="signup-container">
       <form className="signup-form" onSubmit={handleSubmit}>
@@ -8,6 +16,7 @@
           name="email"
           placeholder="Email Address"
           value={formData.email}
+          onChange={(e) => setFormData(e.target.value)}
           required
         />
         <input
@@ -15,6 +24,7 @@
           name="password"
           placeholder="Password"
           value={formData.password}
+          onChange={(e) => setFormData(e.target.value)}
           required
         />
         <button type="submit">Sign Up</button>
