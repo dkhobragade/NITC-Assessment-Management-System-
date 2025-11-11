342c5e6520ae757996c549fec79f9eb7060fd088
dkhobragade <dikshantkhobragade12@gmail.com>
2025-11-12 00:57:44 +0530

    refactor: Clean up import statements and improve code formatting in index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,50 +1,49 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from 'express'
-import { connectDB } from './config/db.js';
-import authRoutes from '../src/routes/authRoutes.js'
-import adminRoutes from '../src/routes/adminRoutes.js'
-import facultyRoutes from '../src/routes/facultyRoutes.js'
-import evaluatorRoutes from '../src/routes/evalutorRoutes.js'
-import studentRoutes from '../src/routes/studentRoutes.js'
-import cookieparser from 'cookie-parser'
-import cors from 'cors'
-import path from 'path'
+import express from "express";
+import { connectDB } from "./config/db.js";
+import authRoutes from "../src/routes/authRoutes.js";
+import adminRoutes from "../src/routes/adminRoutes.js";
+import facultyRoutes from "../src/routes/facultyRoutes.js";
+import evaluatorRoutes from "../src/routes/evalutorRoutes.js";
+import studentRoutes from "../src/routes/studentRoutes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const app = express()
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT;
-
-const __dirname = path.resolve()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(
-  cors( {
-    origin: "http://localhost:5173",
-    methods: [ "GET", "POST", "PUT", "DELETE" ],
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-  } )
-)
-app.use( express.json() )
-app.use( cookieparser() );
+  })
+);
 
-app.use( '/api/auth', authRoutes )
-app.use( '/api/admin', adminRoutes )
-app.use( '/api/faculty', facultyRoutes )
-app.use( '/api/evaluator', evaluatorRoutes )
-app.use( '/api/student', studentRoutes )
+app.use(express.json());
+app.use(cookieParser());
 
-if ( process.env.NODE_ENV === "production" )
-{
-  app.use( express.static( path.join( __dirname, "../client/dist" ) ) )
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/faculty", facultyRoutes);
+app.use("/api/evaluator", evaluatorRoutes);
+app.use("/api/student", studentRoutes);
 
-  app.get( "/*", ( req, res ) =>
-  {
-    res.sendFile( path.join( __dirname, "../client", "dist", "index.html" ) )
-  } )
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
 }
 
-app.listen( PORT, () =>
-{
-  console.log( "Server is running on http://localhost:" + PORT );
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on http://localhost:${PORT}`);
   connectDB();
-} );
+});
