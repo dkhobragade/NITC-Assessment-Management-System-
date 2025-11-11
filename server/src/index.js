4a33cba9b31dcbc2507c83096351bb789c74efd4
dkhobragade <dikshantkhobragade12@gmail.com>
2025-11-12 01:19:09 +0530

    fix: Simplify static file serving and route handling for production environment

--- server/src/index.js
+++ server/src/index.js
@@ -36,12 +36,14 @@ app.use("/api/evaluator", evaluatorRoutes);
 app.use("/api/student", studentRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../client/dist")));
+  const clientPath = path.join(__dirname, "../../client/dist");
+  app.use(express.static(clientPath));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(clientPath, "index.html"));
   });
 }
+
 
 app.listen(PORT, () => {
   console.log(`✅ Server is running on http://localhost:${PORT}`);
